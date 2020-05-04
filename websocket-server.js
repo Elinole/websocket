093284eade6495d4c97211c4b90e9cfa98b5114d
Elias HAMADOUCHE <elinole94@gmail.com>
2020-05-04 13:48:20 +0200

    Initialisation serveur et client websocket : gestion de connexion

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,15 @@
 const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: process.env.WS_PORT });
+
+wss.on('connection', webSocket => {
+    console.log('connection established');
+});
+
+wss.on('close', function close() {
+    console.log('disconnected');
+});
+
+wss.on('error', function close() {
+    console.log('error');
+});
