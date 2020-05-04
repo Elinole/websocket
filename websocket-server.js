48262b2be2080f43570a2573796124a8c77698c0
Elias HAMADOUCHE <elinole94@gmail.com>
2020-05-04 14:04:21 +0200

    Correction serveur websocket

--- websocket-server.js
+++ websocket-server.js
@@ -4,10 +4,10 @@ const wss = new WebSocket.Server({ port: process.env.WS_PORT });
 
 wss.on('connection', webSocket => {
     console.log('connection established');
-});
-
-wss.on('close', function close() {
-    console.log('disconnected');
+    
+    webSocket.on('close', () => {
+        console.log('connection closed by client');
+    });
 });
 
 wss.on('error', function close() {
